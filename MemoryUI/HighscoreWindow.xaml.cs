94cfc396dadd9da19e82270abeab9bfcf67b3948
StefanMaring <smaring@ziggo.nl>
2023-10-26 15:49:15 +0200

    Show ten highest scores working

--- MemoryUI/HighscoreWindow.xaml.cs
+++ MemoryUI/HighscoreWindow.xaml.cs
@@ -21,20 +21,22 @@ namespace MemoryUI
         {
             DataReader dr = new DataReader();
             List<Data> scores = dr.ReadDataFromJSON();
-            var orderedScores = from Data data in scores
+            var orderedScores = (from Data data in scores
                                 orderby data.Score descending
-                                select data;
+                                select data).Take(10);
            
             StackPanel scorePanel = new StackPanel();
             scorePanel.Orientation = Orientation.Vertical;
             scorePanel.HorizontalAlignment = HorizontalAlignment.Center;
             scorePanel.Margin = new Thickness(0,20,0,20);
 
+            int index = 1;
             foreach (Data data in orderedScores)
             {
                 TextBlock score = SetupScoreDisplay();
-                score.Text = $"Speler: {data.PlayerName}, Score: {data.Score}, Aantal kaarten: {data.AmountOfCards}";
+                score.Text = $"#{index} Speler: {data.PlayerName}, Score: {data.Score}, Aantal kaarten: {data.AmountOfCards}";
                 scorePanel.Children.Add(score);
+                index++;
             }
 
             AddChild(scorePanel);
